0621b0e08d3e7ef992c61e0e0396699a32ba666d
ihtblwFIZ <vthyrachel@naver.com>
2025-10-15 11:40:36 +0900

    chore: project setting

---
+++ src/test/java/com/samteam/teammate/TeamMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samteam.teammate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
