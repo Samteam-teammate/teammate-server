8b993c515a7d8b09e7ffec774f4cc1273f1dd223
ihtblwFIZ <vthyrachel@naver.com>
2025-11-02 01:22:30 +0900

    fix: add test yml

--- src/test/java/com/samteam/teammate/TeamMateApplicationTests.java
+++ src/test/java/com/samteam/teammate/TeamMateApplicationTests.java
@@ -2,8 +2,10 @@ package com.samteam.teammate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TeamMateApplicationTests {
 
 	@Test
